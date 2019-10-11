1953c3e80a5f34ce9886063bcaa299b1d54aa930
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-10-11 13:27:55 -0700

    Update count_th.py

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,9 +4,23 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word, count = 0):
+    """ Plan
+    If the length of the param(word) is less than or equal to 1, we just return count
 
-    # if the length of the param is < or equal to 1 return count
+    if
+        return (calls count)
+    elif
+        return (calls count)
+    else
+        return (calls count)
+    """
+
+    """If the length of the passed in parameter(word) is less than or equal to 1, we just return count. """
     if len(word) <= 1:
         return count
+    
+    the_word = word[2:]
+    # Slices the last 2 letters off of the passed in parameter (word)
 
+    pass
     
